30f87fdac0ccfca8b925d86f3d97c1d14e31e5b0
Oliv3r1 <oliver.suomela@metropolia.fi>
2023-09-07 14:48:53 +0300

    hi

---
+++ week1/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      colors: {
+        "os-dark-black": "#05080F",
+        "os-grey": "#131E30",
+        "os-midnight-blue": "#34435A",
+        "os-white": "#FDFEFF",
+        "os-lightblue": "#7F94A9",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
