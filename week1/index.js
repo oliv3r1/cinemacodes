830a91b747dbc7c19740be5e0251f93b4904ccc8
Oliv3r1 <oliver.suomela@metropolia.fi>
2023-09-27 18:46:11 +0300

    viimeistelyt

--- week1/index.js
+++ week1/index.js
@@ -1,11 +1,11 @@
-"use strict";
-
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const path = require("path");
 
-app.use(express.static("public"));
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/src", express.static(path.join(__dirname, "src")));
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
--- week1/public/chat.js
+++ week1/public/chat.js
@@ -1,26 +1,30 @@
 "use strict";
 
-// Server URL below must point to your server, localhost works for local development/testing
 const socket = io("http://localhost:3000");
+
+const nicknameInput = document.getElementById("nickname");
+const roomSelect = document.getElementById("room");
+const messageInput = document.getElementById("m");
 
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-  const inp = document.getElementById("m");
-  socket.emit("chat message", inp.value);
-  inp.value = "";
+  const nickname = nicknameInput.value;
+  const room = roomSelect.value;
+  const message = messageInput.value;
+
+  if (nickname && room && message) {
+    socket.emit("chat message", { nickname, room, message });
+    messageInput.value = "";
+  } else {
+    console.log("Please fill in all fields.");
+  }
 });
 
-socket.on("chat message", (msg) => {
-  const item = document.createElement("li");
-  item.classList.add(
-    "py-2",
-    "px-3",
-    "bg-os-midnight-blue",
-    "rounded-lg",
-    "text-white",
-    "w-fit",
-    "m-3"
-  );
-  item.innerHTML = msg;
-  document.getElementById("messages").appendChild(item);
+socket.on("chat message", (msgObj) => {
+  const { nickname, room, message } = msgObj;
+  if (room === roomSelect.value) {
+    const item = document.createElement("li");
+    item.innerHTML = `<strong>${nickname}:</strong> ${message}`;
+    document.getElementById("messages").appendChild(item);
+  }
 });
